44afdc14ce1ff381907b5bb1e9ad973f836596a9
Dom <122930741+DomHaw21@users.noreply.github.com>
2023-03-09 13:32:18 -0600

    Update main.cpp

--- Assignments/P03/main.cpp
+++ Assignments/P03/main.cpp
@@ -14,14 +14,29 @@
  *    To let the user create a visual representaion of using Graphviz in c++
  *
  * Files: main.cpp
- * Files: test.out
+ * Files: output.txt
  *****************************************************************************/
 #include <iostream>
-#include <map>
 #include <vector>
+#include <map>
+#include <fstream>
 
 using namespace std;
 
+/**
+ * Attribute
+ *
+ * Description:
+ *  allows the creation of an att. map.
+ *
+ * Public Methods:
+ *      Attribute()
+ *      void addAttribute(string key, string val)
+ *      void addAttributes(map<string, string> atts)
+ *
+ * Usage:
+ *   map<string, string> Name;
+ */
 class Attribute {
 protected:
   map<string, string> att; // attributes
@@ -30,130 +45,278 @@ public:
   Attribute() {}
 
   /**
-   * Update an edge attribute. Assumes key is valid.
+   * Updates an edge att
    */
   void addAttribute(string key, string val) { att[key] = val; }
 
   /**
-   * Takes a map of attributes. Loops over them and saves them
+   * Takes the map of the attributes. Saves and stores them
    * locally.
    */
   void addAttributes(map<string, string> atts) {
     for (auto const &x : atts) {
-      cout << x.first << "=" << x.second << endl;
       att[x.first] = x.second;
     }
   }
+
   /**
-   * Use our basic print to dump this class out.
+   *print to dump the class out.
    */
   friend ostream &operator<<(ostream &os, Attribute &e) {
     os << "[";
+
     int i = 0;
     for (auto const &x : e.att) {
       os << x.first << "="
          << "\"" << x.second << "\"";
+
       if (i < e.att.size() - 1) {
         os << ", ";
       }
       i++;
     }
     os << "]";
+
     return os;
   }
 };
 
-
-// Class: Edge (inherits from Attribute)
+/**
+ * Edge
+ *
+ * Description:
+ *    creates the default/overloaded edge
+ *
+ * Public Methods:
+ *      Edge()
+ *    
+ * Private Methods:
+ *      int   eid
+ *      int   start
+ *      int   end
+ *
+ */
 class Edge : public Attribute {
+protected:
+  int eid;
+  int start;
+  int end;
 
 public:
-  // Default Constructor
   Edge() {
-    att["color"] = "black";    // default color
-    att["arrow_type"] = "vee"; // default arrow shape
+    att["color"] = "blue"; // defaults
+    att["arrowhead"] = "vee";
+    att["dir"] = "none";
+    att["arrowtail"] = "tee";
   }
 
-  // Overloaded constructor
-  Edge(string color, string arrow) {
-    att["color"] = color;      // default color 
-    att["arrow_type"] = arrow; // default arrow shape
+  Edge(string color, string arrow, string direction, string arrowtail) {
+    att["color"] = color; // overloads atttibutes
+    att["arrowhead"] = arrow;
+    att["dir"] = direction;
+    att["arrowtail"] = arrowtail;
+  }
+  Edge(int start, int end) {
+    this->start = start;
+    this->end = end;
+  }
+
+  friend ostream &operator<<(ostream &os, Edge &e) {
+    os << e.start << " -> " << e.end << " ";
+    os << "[";
+
+    int i = 0;
+    for (auto const &x : e.att) {
+      os << x.first << "="
+         << "\"" << x.second << "\"";
+
+      if (i < e.att.size() - 1) {
+        os << ", ";
+      }
+      i++;
+    }
+    os << "]";
+
+    return os;
   }
 };
 
-// Class: Node (inherits from Attribute)
+/**
+ * node
+ *
+ * Description:
+ *    Class creates default node and overloads
+ *    it with attributes.
+ *
+ * Public Methods:
+ *    Node(map<string, string> atts)
+ *    Node()
+ */
+
 class Node : public Attribute {
 public:
-  // Default Constructor
   Node() {
-    att["color"] = "black"; // default color
-    att["shape"] = "box";   // default arrow shape
+    att["shape"] = "record"; // default node shape
+    att["color"] = "blue";   // default color
+  }
+
+  Node(map<string, string> atts) {
+    for (auto const &x : atts) {
+      att[x.first] = x.second;
+    }
   }
 };
 
-// Class: GraphViz
-class GraphViz {
-  int edgeId;          // initialize Edge id
-  int nodeId;          // initialize Node id
-  vector<Edge> Edges;  // creates a vector of Edges 
-  vector<Node> Nodes;  // creates a vector of Nodes 
-
-  // Create an Edge Id using start and end points of the edge
-  string makeEid(int start, int end) { return to_string(start) + "-" + to_string(end); }
+/**
+ * graphViz
+ *
+ * Description:
+ *      Creates an instance of graphviz, can edit nodes and edges.
+ *      And it also adds them if neccessary
+ *
+ * Public Methods:
+ *      GraphViz()
+ *      int addNode()
+ *      void editNode(int id, string key, string value)
+ *      void addEdge(int start, int end, map<string, string> att)
+ *      void editEdge(int id, string key, string value)
+ *
+ *
+ * Private Methods:
+ *        int           nodeId;
+ *        int           edgeId;
+ *        vector<Node>  Nodes;
+ *        vector<Edge>  Edges;
+ * Usage:
+ *        graphViz G()
+ *        G.addNode()
+ *        G.addNode(att)
+ *        G.editNode(num, string, string)
+ *        G.addEdge(num, num)
+ *        G.addEdge(num, num, att)
+ *        G.editEdge(num, string, string)
+ *
+ */
+class graphViz {
+  int nodeId;
+  int edgeId;
+  vector<Node> Nodes;
+  vector<Edge> Edges;
 
 public:
-  // Default Constructor
-  GraphViz() {
-    edgeId = 0;
+  graphViz() {
     nodeId = 0;
+    edgeId = 0;
   }
-
-  // Function to add node to vector of Nodes
+  //default
   int addNode() {
-    // Pushes 
     Nodes.push_back(Node());
     return Nodes.size() - 1;
   }
+  // gives the ability to add nodes
+  int addNode(map<string, string> atts) {
+    Nodes.push_back(Node(atts));
 
-  // Function to add edge to vector of Edges
-  void addEdge(int start, int end) { 
-    Edges.push_back(Edge()); 
+    return Nodes.size() - 1;
   }
+  // gives ability to edit a node
+  void editNode(int id, string key, string value) {
+    Nodes[id].addAttribute(key, value);
+  }
+  // gives ability to add edges
+  void addEdge(int start, int end) { Edges.push_back(Edge(start, end)); }
+  // gives ability to add edges
+  void addEdge(int start, int end, map<string, string> att) {
 
-  // Output your graphviz stuff
-  void printGraph(string graphName) {
-    cout << "digraph" << graphName << "{";
-    // loop over nodes and print them
-    // loop over edges and print them
-    cout << "}\n";
+    Edges.push_back(Edge(start, end));
+    Edges[Edges.size() - 1].addAttributes(att);
+  }
+  // gives ability to edit an edge
+  void editEdge(int id, string key, string value) {
+    Edges[id].addAttribute(key, value);
+  }
+  //prints the template of graphviz
+  friend ostream &operator<<(ostream &os, graphViz &G) {
+    os << "digraph "
+       << "linkedList"
+       << " {\n";
+    for (int i = 0; i < G.Nodes.size(); i++) {
+      os << i << " " << G.Nodes[i] << endl;
+    }
+    for (int i = 0; i < G.Edges.size(); i++) {
+      os << G.Edges[i] << endl;
+    }
+    os << "}\n";
+
+    return os;
   }
 };
 
 int main() {
-  // This is a hint to include some tracking variables
-  int start;
-  int end;
+  ofstream outfile;
+  outfile.open("output.txt");
 
-  // Example of Vector that holds nodes
-  vector<Node> Nodes;
+  // outputting heading
+  outfile << "Dominique Hawkins\n";
+  outfile << "2143\n";
+  outfile << "OOP\n\n";
 
-  // We know that the index of the vector = 0-3
-  Nodes.push_back(Node());
-  Nodes.push_back(Node());
-  Nodes.push_back(Node());
-  Nodes.push_back(Node());
+  // Creates an instance
+  graphViz V;
 
-  // Adds attributes to the nodes using the index of the vector
-  Nodes[2].addAttribute("fillColor","purple");
-  Nodes[3].addAttribute("fillColor", "red");
+  // Defaults for both edge/node atts
+  map<string, string> defEdge;
+  
+  map<string, string> defNode;
+  // Attribute that gets a map of attributes and
+  Attributes A;
+  defEdge["color"] = "red";
+  defEdge["arrowhead"] = "vee";
+  A.addAttributes(defEdge);
+  
+  defNode["shape"] = "record";
+  defNode["color"] = "black";
+  A.addAttributes(defNode);
 
-  // Example output of dot code
-  cout << "digraph" << " Pie" << "{\n";
-    // Loops through vector of Nodes and outputs to console
-    for(int i=0;i<Nodes.size();i++){
-      cout<<"\t"<<i<<" "<<Nodes[i]<<endl;
-    }
-  cout << "}\n";
- 
-  return 0;
+
+  // makes the linked list nodes and adds the attributes
+  int nodeId = V.addNode(defNode);
+  nodeId = V.addNode();
+  V.editNode(1, "shape", "circle");
+  V.editNode(1, "color", "red");
+  V.editNode(1, "label", "4");
+  nodeId = V.addNode();
+  V.editNode(2, "shape", "star");
+  V.editNode(2, "color", "yellow");
+  V.editNode(2, "style", "solid");
+  nodeId = V.addNode();
+  V.editNode(3, "shape", "pentagon");
+  V.editNode(3, "color", "black");
+  V.editNode(3, "style", "invisible");
+  nodeId = V.addNode(defNode);
+  V.editNode(4, "label", "{ <data> 56 | <next> }");
+
+  
+ //makes the edges and edits them
+  V.addEdge(0, 1, defEdge);
+  V.editEdge(0, "color", "green");
+  V.editEdge(0, "arrowhead", "crow");
+  V.editEdge(0, "arrowtail", "dot");
+  V.editEdge(0, "dir", "both");
+  V.addEdge(1, 2, defEdge);
+  V.editEdge(1, "color", "green");
+  V.editEdge(1, "arrowhead", "curve");
+  V.addEdge(2, 3, defEdge);
+  V.editEdge(2, "color", "black");
+  V.editEdge(2, "arrowhead", "icurve");
+  V.editEdge(2, "arrowtail", "none");
+  V.editEdge(2, "dir", "both");
+  V.addEdge(3, 4, defEdge);
+  V.editEdge(3, "color", "brown");
+  V.editEdge(3, "dir", "none");
+
+  //prints the output file
+  outfile << V << endl;
+  //closes outfile
+  outfile.close();
 }
